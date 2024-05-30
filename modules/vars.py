ebacdb632966772dea903c6aee20bd745978e56c
ankit669281 <165649771+ankit669281@users.noreply.github.com>
2024-05-30 07:48:08 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "26398147"
 api_hash = "050ae2e744985b48fa37e8b2f38d7bea"
-bot_token = "7143613089:AAGRhVPZJdwbKOEXqeKV80imLjXDGWlGAnc"
+bot_token = "7435452455:AAHDAJqa_S-MRUauVvUg6eOOO9jw_5K5tXo"
